abcda24c49a62bf91ef3d415f95819346ee0b713
makercode <makercode@gmail.com>
2023-02-02 19:29:32 -0500

    fixed product save callback

--- admin/app/listeners.php
+++ admin/app/listeners.php
@@ -101,111 +101,112 @@ function action_woocommerce_new_and_update_product( $postId ) {
 	require_once dirname(__file__).'/../../_business/api/products.api.php';
 	global $wpdb;
 
+	$post = get_post( $postId );
+	$product = wc_get_product( $postId );
+
 	$validatedGuard = new ValidatedGuard;
 	$choosedGuard = new ChoosedGuard;
 
 	if( $validatedGuard->isValidated()=="1" && $choosedGuard->isChoosed() ) {
 
-		$post = get_post( $postId );
-		$product = wc_get_product( $postId );
+		if( get_post_type($post)=="product" ) {
 
-		$noValidTypes = !( $product->is_type( 'grouped' )||$product->is_type( 'external' ) );
+			if ( $product->is_type( 'simple' ) ) {
 
-		if ( get_post_type( $post ) == 'product' && $noValidTypes && !($product->is_type( 'variable' )) ) {
+				// for variable products
+				$message = __( 'GUARDADO COMO BORRADOR. Producto: El SKU es obligatorio para sincronizar en Starsoft.', 'woocommerce' );
+				$message_no_found = __( 'GUARDADO COMO BORRADOR. Producto: El SKU que elegiste no se encontró en Starsoft.', 'woocommerce' );
 
-			// for variable products
-			$message = __( 'GUARDADO COMO BORRADOR. Producto: El SKU es obligatorio para sincronizar en Starsoft.', 'woocommerce' );
-			$message_no_found = __( 'GUARDADO COMO BORRADOR. Producto: El SKU que elegiste no se encontró en Starsoft.', 'woocommerce' );
-
-			$product_sku = $product->get_sku();
+				$product_sku = $product->get_sku();
 
 
-			if( !$product->get_sku() ) {
-				remove_action( 'save_post', 'action_woocommerce_new_and_update_product' );
-				wp_update_post( array( 'ID' => $postId, 'post_status' => 'draft' ) );
-				add_action( 'save_post', 'action_woocommerce_new_and_update_product' );
-				WC_Admin_Meta_Boxes::add_error( $message );
-				return;
-			}
-			
-			$productsApi = new ProductsApi;
-
-			$productsStack = array();
-			$productSkuDTO = new ProductSkuDTO( $product_sku );
-			array_push($productsStack, $productSkuDTO);
-
-			$isProductInStarsoft = $productsApi->verifyProducts( $productsStack );
-
-			if( !$isProductInStarsoft ) {
-				remove_action( 'save_post', 'action_woocommerce_new_and_update_product' );
-				wp_update_post( array( 'ID' => $postId, 'post_status' => 'draft' ) );
-				add_action( 'save_post', 'action_woocommerce_new_and_update_product' );
-				WC_Admin_Meta_Boxes::add_error( $message_no_found );
-				return;
-			}
-
-			$isProductInStarsoft_obj = json_decode($isProductInStarsoft);
-			if(!$isProductInStarsoft_obj[0]->Exists) {
-				remove_action( 'save_post', 'action_woocommerce_new_and_update_product' );
-				wp_update_post( array( 'ID' => $postId, 'post_status' => 'draft' ) );
-				add_action( 'save_post', 'action_woocommerce_new_and_update_product' );
-				WC_Admin_Meta_Boxes::add_error( $message_no_found );
-				return;
-			}
-
-		}
-
-		if( $product->is_type( 'variable' ) ) {
-
-			$message = __( 'GUARDADO COMO BORRADOR. Producto: El SKU es obligatorio para sincronizar en Starsoft.', 'woocommerce' );
-
-			$parentProductId = $product->get_parent_id();
-
-			// var_dump("product");
-
-			// only if product variable is the father, child are listened in another function above
-			if( $parentProductId == 0 ) {
-				// var_dump("entered");
-				// var_dump($parentProductId);
-				$productsStack = array();
-				foreach ( $product->get_children() as $child_id ) {
-					// get an instance of the WC_Variation_product Object
-					$variation = wc_get_product( $child_id ); 
-
-
-					$variation_sku = $variation->get_sku();
-
-
-					if( !$variation->get_sku() ) {
-						remove_action( 'save_post', 'action_woocommerce_new_and_update_product' );
-						wp_update_post( array( 'ID' => $postId, 'post_status' => 'draft' ) );
-						add_action( 'save_post', 'action_woocommerce_new_and_update_product' );
-						WC_Admin_Meta_Boxes::add_error( $message );
-						return;
-					}
-					
-
-					$variationSkuDTO = new ProductSkuDTO( $variation_sku );
-					array_push( $productsStack, $variationSkuDTO );
+				if( !$product->get_sku() ) {
+					remove_action( 'save_post', 'action_woocommerce_new_and_update_product' );
+					wp_update_post( array( 'ID' => $postId, 'post_status' => 'draft' ) );
+					add_action( 'save_post', 'action_woocommerce_new_and_update_product' );
+					WC_Admin_Meta_Boxes::add_error( $message );
+					return;
 				}
-
+				
 				$productsApi = new ProductsApi;
-				$areProductsInStarsoft = $productsApi->verifyProducts( $productsStack );
-				$areProductsInStarsoft_obj = json_decode( $areProductsInStarsoft );
 
-				foreach ( $areProductsInStarsoft_obj as $isProductInStarsoft ) {
+				$productsStack = array();
+				$productSkuDTO = new ProductSkuDTO( $product_sku );
+				array_push($productsStack, $productSkuDTO);
 
-					if(!$isProductInStarsoft->Exists) {
-						remove_action( 'save_post', 'action_woocommerce_new_and_update_product' );
-						wp_update_post( array( 'ID' => $postId, 'post_status' => 'draft' ) );
-						add_action( 'save_post', 'action_woocommerce_new_and_update_product' );
-						$message = 'GUARDADO COMO BORRADOR. Producto: El SKU '.$isProductInStarsoft->SKU.' no se encontró en Starsoft.';
-						WC_Admin_Meta_Boxes::add_error( $message );
-						return;
-					}
+				$isProductInStarsoft = $productsApi->verifyProducts( $productsStack );
+
+				if( !$isProductInStarsoft ) {
+					remove_action( 'save_post', 'action_woocommerce_new_and_update_product' );
+					wp_update_post( array( 'ID' => $postId, 'post_status' => 'draft' ) );
+					add_action( 'save_post', 'action_woocommerce_new_and_update_product' );
+					WC_Admin_Meta_Boxes::add_error( $message_no_found );
+					return;
 				}
-			} 
-			
+
+				$isProductInStarsoft_obj = json_decode($isProductInStarsoft);
+				if(!$isProductInStarsoft_obj[0]->Exists) {
+					remove_action( 'save_post', 'action_woocommerce_new_and_update_product' );
+					wp_update_post( array( 'ID' => $postId, 'post_status' => 'draft' ) );
+					add_action( 'save_post', 'action_woocommerce_new_and_update_product' );
+					WC_Admin_Meta_Boxes::add_error( $message_no_found );
+					return;
+				}
+
+			}
+
+			if( $product->is_type( 'variable' ) ) {
+
+				$message = __( 'GUARDADO COMO BORRADOR. Producto: El SKU es obligatorio para sincronizar en Starsoft.', 'woocommerce' );
+
+				$parentProductId = $product->get_parent_id();
+
+				// var_dump("product");
+
+				// only if product variable is the father, child are listened in another function above
+				if( $parentProductId == 0 ) {
+					// var_dump("entered");
+					// var_dump($parentProductId);
+					$productsStack = array();
+					foreach ( $product->get_children() as $child_id ) {
+						// get an instance of the WC_Variation_product Object
+						$variation = wc_get_product( $child_id ); 
+
+
+						$variation_sku = $variation->get_sku();
+
+
+						if( !$variation->get_sku() ) {
+							remove_action( 'save_post', 'action_woocommerce_new_and_update_product' );
+							wp_update_post( array( 'ID' => $postId, 'post_status' => 'draft' ) );
+							add_action( 'save_post', 'action_woocommerce_new_and_update_product' );
+							WC_Admin_Meta_Boxes::add_error( $message );
+							return;
+						}
+						
+
+						$variationSkuDTO = new ProductSkuDTO( $variation_sku );
+						array_push( $productsStack, $variationSkuDTO );
+					}
+
+					$productsApi = new ProductsApi;
+					$areProductsInStarsoft = $productsApi->verifyProducts( $productsStack );
+					$areProductsInStarsoft_obj = json_decode( $areProductsInStarsoft );
+
+					foreach ( $areProductsInStarsoft_obj as $isProductInStarsoft ) {
+
+						if(!$isProductInStarsoft->Exists) {
+							remove_action( 'save_post', 'action_woocommerce_new_and_update_product' );
+							wp_update_post( array( 'ID' => $postId, 'post_status' => 'draft' ) );
+							add_action( 'save_post', 'action_woocommerce_new_and_update_product' );
+							$message = 'GUARDADO COMO BORRADOR. Producto: El SKU '.$isProductInStarsoft->SKU.' no se encontró en Starsoft.';
+							WC_Admin_Meta_Boxes::add_error( $message );
+							return;
+						}
+					}
+				} 
+				
+			}
 		}
 	}
 
